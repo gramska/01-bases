8fce5c3823b8275cb96a3e365b9631110a085792
gramska <gabinoramos@gmail.com>
2023-01-27 23:19:59 -0600

    commit para actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+
+  titulo: string = 'Contador App';
+  numero: number = 0;
+  base: number = 2;
+
+  acumular(valor: number): void{
+    this.numero += valor;
+  }
 }
